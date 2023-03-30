152f9fe08f7a03945b330f9c44523eb53582e026
Wender <wendermaiac@gmail.com>
2023-03-30 08:05:36 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine($"Que ano você nasceu?");
+int anoAniversario = int.Parse(Console.ReadLine());
+
+int ano = DateTime.Today.Year - anoAniversario;
+
+float semanas = ano * 52.1786f; 
+
+Console.WriteLine($"Você tem {ano} anos, que corresponde à {Math.Round(semanas)} semanas");
